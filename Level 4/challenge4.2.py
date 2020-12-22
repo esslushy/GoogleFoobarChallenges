5f14c067cefe7645cec08f8b3d8b869eb93ea4de
esslushy <esslushy@gmail.com>
2020-12-22 00:26:57 -0600

    finished dfs for optimal path, works on test cases

--- Level 4/challenge4.2.py
+++ Level 4/challenge4.2.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from itertools import combinations, permutations
 
 def solution(times, times_limit):
     # Special case if there are no intermediary bunnies to pick up.
@@ -12,8 +13,8 @@ def solution(times, times_limit):
         # If a negative cycle exists, all bunnies can be picked up, so return a list of all prisoner ids
         # Prisoner ids start from 0 and there are 2 less than the full set of times (no start or bulkhead)
         return list(range(len(times)-2))
-    # If no negative cycles, do dfs
-    return new_times
+    # If no negative cycles, do dfs to find the most bunnies one can pick up.
+    return get_most_bunnies(new_times, times_limit)
 
 def floyd_warshall(graph):
     """
@@ -47,6 +48,59 @@ def negative_cycles(graph):
         if graph[i][i] < 0:
             return True
     return False
+
+def create_subsets(main_set):
+    """
+      Creates all possible subsets of the main set
+
+      Args:
+        main_set: the set to create subsets of
+    """
+    # Initialize subsets
+    subsets = []
+    # Use itertools to create all combinations of each possible length
+    for n in range(1, len(main_set)+1):
+        subsets.extend(combinations(main_set, n))
+    return subsets
+
+def get_most_bunnies(graph, time_limit):
+    """
+      Uses DFS to check each possible collection of bunnies to see which one is the most optimal.
+
+      Args:
+        graph: the graph that can be checked over.
+    """
+    # Initialize list of bunnies. -2 to skip entrance and exit
+    bunny_ids = list(range(len(graph)-2))
+    # Create all subsets to try
+    bunny_sets = create_subsets(bunny_ids) # Using tuples, but should be fine as long as it returns a list at the end
+    # Sort the subsets so they are in order of lowest prisoner id to highest
+    bunny_sets = sorted(bunny_sets)
+    # Initial optimal_bunny_set variable
+    optimal_bunny_set = ()
+    # Test each subset
+    for subset in bunny_sets:
+        # Test each permutation of the subset. AKA any order of picking up the bunnies
+        for permutation in permutations(subset):
+            # Initialize variable to track and accumulate distance
+            distance = 0
+            previous_bunny = 0 # 0 is the start with no bunny
+            # Iter through picking up each bunny
+            for bunny_id in permutation:
+                # Set up next_bunny to move to. 1 is added because bunny ids are 1 behind there actual location in graph
+                next_bunny = bunny_id + 1
+                # Add distance from previous bunny to next
+                distance += graph[previous_bunny][next_bunny] 
+                # Set previous bunny to the bunny just moved to
+                previous_bunny = next_bunny
+            # Add last iteration from final bunny to end
+            distance += graph[previous_bunny][len(graph)-1]
+            # Make sure distance passes checks
+            if distance <= time_limit and len(subset) > len(optimal_bunny_set):
+                # If it does, make the current subset the next optimal subset
+                optimal_bunny_set = subset
+    # Cast optimal_bunny_set to list so it can pass checks
+    return list(optimal_bunny_set)
 
 print(solution([[0, 2, 2, 2, -1], [9, 0, 2, 2, -1], [9, 3, 0, 2, -1], [9, 3, 2, 0, -1], [9, 3, 2, 2, 0]], 1))
 # Expected: [1, 2]
